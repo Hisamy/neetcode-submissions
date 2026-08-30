3e89a4bb273e4032648d6fc186fface81e8fbdd6
Hisamy Cinco Cota <159646604+Hisamy@users.noreply.github.com>
2026-08-30 13:18:47 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+
+        mapS, mapT = {}, {}
+
+        for i in range(len(s)):
+            mapS[s[i]] = mapS.get(s[i], 0) + 1
+            mapT[t[i]] = mapT.get(t[i], 0) + 1
+        
+        for i in mapS:
+            if i not in mapT or mapS.get(i) != mapT.get(i): 
+                return False
+
+        return True
+
+
+
+            
+
+            
+
+        
+        
+        
